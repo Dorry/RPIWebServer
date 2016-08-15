0a56ccbdbeb0ffbc9b394e3fcc6b1d415cc50a1b
Park Kyungbong <dorry457@nate.com>
2016-08-15 19:05:38 +0900

    process kill added

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ def getCPU_Temperature():
     return "0'C"
 
 def isCameraOn():
-    camon_list = subprocess.check_output("ps -ef | egrep 'raspivid|exam' | grep -v grep", shell=True)
-    if len(camon_list) > 0:
+    try:
+        camon_list = subprocess.check_output("ps -ef | egrep 'raspivid|exam' | grep -v grep", shell=True)
         return True
-    return False
+    except subprocess.CalledProcessError as e:
+        return False
 
 app = Flask(__name__)
 
@@ -43,7 +44,7 @@ def logout():
 
 @app.route('/camon')
 def camon():
-    subprocess.call("sudo nohup /home/pi/pi_cam.sh &")
+    os.system("nohup /home/pi/pi_cam.sh | python3 /home/pi/exam.py &")
     context = {'isoff': True,
                'msg': "카메라가 실행되지 않았습니다"}
     if isCameraOn():
@@ -54,19 +55,27 @@ def camon():
 
 @app.route('/camoff')
 def camoff():
+    result = False
+    context = {'isoff': False,
+               'msg': "카메라가 종료되지 않았습니다"}
     res_list = subprocess.check_output("ps -ef | egrep 'raspivid|exam' | grep -v grep | awk '{print $2}'", shell=True)
     if len(res_list) > 0:
         result = True
-        res_list = str(res_list).splitlines()
-    else:
-        result = False
 
-    context = {'isoff': False,
-               'msg': "카메라가 종료되지 않았습니다"}
     if result:
-        res_list = [int(str(pid).replace('\\n', '')) for pid in res_list]
-        kill_res = subprocess.call("sudo kill -9 %s" % " ".join(res_list))
-        if not kill_res and not isCameraOn():
+        print(res_list)
+        res_list = str(res_list).split('\\n')
+        res_list1 = []
+        for pid in res_list:
+            pid = str(pid).strip().replace("b'", '')
+            pid = pid.replace("'", '')
+            res_list1.append(pid)
+        res_list1 = " ".join(res_list1)
+        res_list1 = res_list1.strip()
+        for pid in res_list1.split(" "):
+            os.kill(int(pid), 9)
+
+        if not isCameraOn():
             context['isoff'] = True
             context['msg'] = "카메라가 정상종료 되었습니다"
     return render_template('cam_status.html', **context)
