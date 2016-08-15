2fdf29d58bffc61c32504dbfe263c858c500f148
Park Kyungbong <dorry457@nate.com>
2016-08-15 16:05:01 +0900

    cam status info added

--- app.py
+++ app.py
@@ -12,12 +12,19 @@ def getCPU_Temperature():
         return temp
     return "0'C"
 
+def isCameraOn():
+    camon_list = subprocess.check_output("ps -ef | egrep 'raspivid|exam' | grep -v grep", shell=True)
+    if len(camon_list) > 0:
+        return True
+    return False
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    cpu_temp = getCPU_Temperature()
-    return render_template('index.html', cpu_temp=cpu_temp)
+    context = {'cpu_temp': getCPU_Temperature(),
+               'isoff': not isCameraOn()}
+    return render_template('index.html', context)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,10 +41,33 @@ def logout():
     session.pop('login_ok', None)
     return redirect(url_for('index'))
 
+@app.route('/camon')
+def camon():
+    subprocess.call("nohup /home/pi/pi_cam.sh &")
+    context = {'isoff': True,
+               'msg': "카메라가 실행되지 않았습니다"}
+    if isCameraOn():
+        context['isoff'] = False
+        context['msg'] = "카메라가 정상실행되었습니다"
+    return render_template('cam_status.html', context)
+
+
+@app.route('/camoff')
+def camoff():
+    result = subprocess.call("kill -9 `ps -ef | egrep 'raspivid|exam' | grep -v grep | awk 'print $2'`")
+    context = {'isoff': False,
+               'msg': "카메라가 종료되지 않았습니다"}
+    if not result:
+        if not isCameraOn():
+            context['isoff'] = True
+            context['msg'] = "카메라가 정상종료 되었습니다"
+    return render_template('cam_status.html', context)
+
 @app.route('/restart')
 def restart():
     if session['login_ok']:
         if os.name != 'nt':
+            session.pop('login_ok', None)
             os.system('sudo reboot now')
         return 'Now rebooting at Server!'
     return redirect(url_for('login'))
